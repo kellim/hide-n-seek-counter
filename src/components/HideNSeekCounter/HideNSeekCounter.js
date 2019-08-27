c0e5dd1fcf4546f57a4bebbe689c47571de31766
Kelli Blalock <kelli.blalock2@gmail.com>
2019-08-26 20:29:09 -0700

    Add time's up audio

--- src/components/HideNSeekCounter/HideNSeekCounter.js
+++ src/components/HideNSeekCounter/HideNSeekCounter.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './HideNSeekCounter.css'
 import NumberInput from '../NumberInput'
 import SecondsCounter from '../SecondsCounter'
+import timesUpAudio from '../../audio/timesup.mp3'
 
 class HideNSeekCounter extends Component {
 
@@ -13,16 +14,17 @@ class HideNSeekCounter extends Component {
       // Possible Statuses: setup, counting, done
       appStatus: "setup"
     }
+    this.audioEl = React.createRef()
     this.updateNumberInput = this.updateNumberInput.bind(this)
     this.processSetupForm = this.processSetupForm.bind(this)
     this.handleRestartBtnClick = this.handleRestartBtnClick.bind(this)
     this.handleReturnBtnClick = this.handleReturnBtnClick.bind(this)
     this.secondsTimer = null
-
   }
 
   componentWillUnmount() {
     clearInterval(this.secondsTimer)
+    this.resetAudio()
   }
 
   updateNumberInput(evt) {
@@ -30,6 +32,9 @@ class HideNSeekCounter extends Component {
   }
 
   handleRestartBtnClick() {
+    if (this.state.appStatus === "done") {
+      this.resetAudio()
+    }
     clearInterval(this.secondsTimer)
     this.setState({ 
       currentCount: 0,
@@ -39,6 +44,9 @@ class HideNSeekCounter extends Component {
   }
 
   handleReturnBtnClick() {
+    if (this.state.appStatus === "done") {
+      this.resetAudio()
+    }
     clearInterval(this.secondsTimer)
     this.setState({ 
       appStatus: "setup",
@@ -58,6 +66,7 @@ class HideNSeekCounter extends Component {
         currentCount: st.currentCount + 1
       }), function() {
         if (this.state.currentCount >= this.state.inputValue) {
+          this.audioEl.current.play()
           clearInterval(this.secondsTimer)
           this.setState({ appStatus: "done" })
         }
@@ -65,15 +74,21 @@ class HideNSeekCounter extends Component {
     }, 1000)
   }
 
+  resetAudio() {
+    this.audioEl.current.pause()
+    this.audioEl.current.currentTime = 0
+  }
+
   render() {
     const { inputValue, currentCount, appStatus } = this.state
     return (
       <div className="HideNSeekCounter">
+        <audio ref={this.audioEl} src={timesUpAudio} />
         <h1 className="HideNSeekCounter-title">Hide & Seek Counter</h1>
       {appStatus === "setup" ?
         <>
           <p className="HideNSeekCounter-setup-text">To the person who's not hiding: Enter the seconds to count up to and click
-            the <strong>Start Counting</strong> button. You'll hear a sound when it's time to say "Ready or not, here I come!" so make sure your volume is on.</p> 
+            the <strong>Start Counting</strong> button. When the time is up, you'll hear "Time's up! Ready or not here I come" so make sure your volume is on.</p> 
           <NumberInput countValue={inputValue} handleChange={this.updateNumberInput} handleSubmit={this.processSetupForm} />
         </>
       :
